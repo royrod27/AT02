e04461eb05437a4c1b2b089f6312cddd8b5c05b2
Roy Rodriguez Montecinos <RoyRodriguez@Fundacion-jala.local>
2016-08-12 16:57:03 -0400

    Task_09(update)

---
+++ Roy/Ruby/Day 9/Practice/Prac_09.02.rb
@@ -0,0 +1,17 @@
+cars = {
+	'altima'	=>	'nissan',
+	'camry'		=>	'toyota',
+	'rx7'		=>	'mazda'
+}
+puts cars['rx7']
+
+elements = {}
+elements['H'] = 'Hydrogen'
+elements['He'] = 'Helium'
+elements['Li'] = 'Lithium'
+
+p elements['H']
+p elements.length
+p elements.values
+p elements.keys
+p elements
